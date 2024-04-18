6b8cd25ee638bed833558ce1417dac6303310322
MajesticNthg <majestic_nthg@mail.ru>
2024-04-18 15:11:08 +1000

    Create project MachinePatternRecognition

--- src/Level1.java
+++ src/Level1.java
@@ -1,17 +1,11 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Level1 {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static boolean LineAnalysis(String line) {
+        if (line.charAt(0) != '*' && line.charAt(line.length() - 1) != '*') return false;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        for (int x = 0; x < line.length() / 2; x++) {
+            if (line.charAt(x) != line.charAt(line.length() - x - 1)) return false;
         }
+        return true;
     }
 }
+
